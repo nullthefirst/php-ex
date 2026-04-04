c99b0cdcd311efd9bc6107bd8a95589188a29a00
Usheninte Dangana <usheninte234@gmail.com>
2026-04-04 10:23:45 +0100

    Added lasagna exercise

---
+++ lasagna/Lasagna.php
@@ -0,0 +1,29 @@
+<?php
+
+class Lasagna
+{
+    public function expectedCookTime()
+    {
+        // Implement the expectedCookTime method
+    }
+
+    public function remainingCookTime($elapsed_minutes)
+    {
+        // Implement the remainingCookTime method
+    }
+
+    public function totalPreparationTime($layers_to_prep)
+    {
+        // Implement the totalPreparationTime method
+    }
+
+    public function totalElapsedTime($layers_to_prep, $elapsed_minutes)
+    {
+        // Implement the totalElapsedTime method
+    }
+
+    public function alarm()
+    {
+        // Implement the alarm method
+    }
+}
